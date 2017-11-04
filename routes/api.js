4248920ff43581382674675a2dc551c25d2bf42b
Michael Hatmaker <Michael.Hatmaker@gmail.com>
2017-11-04 13:30:42 -0700

    try again on disabling access control origin *

--- routes/api.js
+++ routes/api.js
@@ -49,7 +49,7 @@ function loadHeaders(req, res) {
  * Headers
  */
     res.header("Access-Control-Allow-Credentials", responseSettings.AccessControlAllowCredentials);
-    res.header("Access-Control-Allow-Origin",  responseSettings.AccessControlAllowOrigin);
+    // res.header("Access-Control-Allow-Origin",  responseSettings.AccessControlAllowOrigin);
     res.header("Access-Control-Allow-Headers", (req.headers['access-control-request-headers']) ? req.headers['access-control-request-headers'] : "x-requested-with");
     res.header("Access-Control-Allow-Methods", (req.headers['access-control-request-method']) ? req.headers['access-control-request-method'] : responseSettings.AccessControlAllowMethods);
 
@@ -107,7 +107,7 @@ exports.getUserName = function (req, res) {
         console.log("reset seqNo to zero");
     }
     console.log("return seqNo %s, name %s", seqNo, userNames[seqNo].name);
-    // loadHeaders(req, res);
+    loadHeaders(req, res);
     res.json({'id' : seqNo, 'name' : userNames[seqNo].name});
     seqNo++;
 };
