3935a8b6f43ad1288c9f49a39c30e0312b9f57e5
Michael Hatmaker <Michael.Hatmaker@gmail.com>
2017-11-03 17:28:28 -0700

    initial addition of CORS header handling

--- routes/api.js
+++ routes/api.js
@@ -37,6 +37,32 @@ var pusherRef = null,
         'port': "3035"
     };
 
+function loadHeaders(req, res) {
+    var responseSettings = {
+        "AccessControlAllowOrigin": req.headers.origin,
+        "AccessControlAllowHeaders": "Content-Type,X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5,  Date, X-Api-Version, X-File-Name",
+        "AccessControlAllowMethods": "POST, GET, PUT, DELETE, OPTIONS",
+        "AccessControlAllowCredentials": true
+    };
+
+/**
+ * Headers
+ */
+    res.header("Access-Control-Allow-Credentials", responseSettings.AccessControlAllowCredentials);
+    res.header("Access-Control-Allow-Origin",  responseSettings.AccessControlAllowOrigin);
+    res.header("Access-Control-Allow-Headers", (req.headers['access-control-request-headers']) ? req.headers['access-control-request-headers'] : "x-requested-with");
+    res.header("Access-Control-Allow-Methods", (req.headers['access-control-request-method']) ? req.headers['access-control-request-method'] : responseSettings.AccessControlAllowMethods);
+
+    // if ('OPTIONS' == req.method) {
+    //     res.send(200);
+    // }
+    // else {
+    //     console.log("Fell through loadHeaders to next() call");
+        // next();
+    // }
+
+}
+
 exports.getAuth = function (req, res) {
     "use strict";
     console.log('getAuth');
@@ -46,6 +72,7 @@ exports.getAuth = function (req, res) {
     var socketId = req.body.socket_id,
         channel = req.body.channel_name,
         auth = pusherRef.authenticate(socketId, channel);
+    loadHeaders(req, res);
     res.send(auth);
 };
 
@@ -67,6 +94,7 @@ exports.setHostEnvironment = function (host, port) {
 exports.getHostEnvironment = function (req, res) {
     console.log("getHostEnvironment returning");
     console.log(env.host + ", " + env.port);
+    loadHeaders(req, res);
     res.json(env);
 }
 
@@ -79,6 +107,7 @@ exports.getUserName = function (req, res) {
         console.log("reset seqNo to zero");
     }
     console.log("return seqNo %s, name %s", seqNo, userNames[seqNo].name);
+    loadHeaders(req, res);
     res.json({'id' : seqNo, 'name' : userNames[seqNo].name});
     seqNo++;
 };
@@ -93,6 +122,7 @@ exports.getUserId = function (req, res) {
         console.log("reset seqNo to zero");
     }
     console.log("return seqNo %s ", seqNo);
+    loadHeaders(req, res);
     res.json({'id' : seqNo});
     seqNo++;
 };
@@ -103,5 +133,6 @@ exports.getNextWindowSeqNo = function (req, res) {
 
     console.log("return wndNameSeqNo %s ", wndNameSeqNo);
     res.json({'wndNameSeqNo' : wndNameSeqNo});
+    loadHeaders(req, res);
     wndNameSeqNo++;
 };
