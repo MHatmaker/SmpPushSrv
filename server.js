b19d9d005eddffb7211183006afe1761a1aaad2f
Michael Hatmaker <Michael.Hatmaker@gmail.com>
2018-08-27 14:51:34 -0700

    starts implementing email post handler

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ var express = require('express'),
     path = require('path'),
     socketio  = require('socket.io'),
     Pusher = require('pusher'),
+    nodeMailer = require('nodemailer'),
     pusher,
     MasherNodeAppj,
     routesHtml,
@@ -243,6 +244,51 @@ urls = (
             res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
             next();
         });
+
+        self.app.post('/send-email', function (req, res) {
+            nodemailer = require('nodemailer');
+
+            // Generate SMTP service account from ethereal.email
+            nodemailer.createTestAccount((err, account) => {
+                if (err) {
+                    console.error('Failed to create a testing account. ' + err.message);
+                    return process.exit(1);
+                }
+
+                console.log('Credentials obtained, sending message...');
+
+                // Create a SMTP transporter object
+                let transporter = nodemailer.createTransport({
+                    host: account.smtp.host,
+                    port: account.smtp.port,
+                    secure: account.smtp.secure,
+                    auth: {
+                        user: account.user,
+                        pass: account.pass
+                    }
+                });
+
+                // Message object
+                let message = {
+                    from: 'MapLinkr <mapsyncr@gmail.com>',
+                    to: req.body.to,
+                    subject: 'MapLinkr shared map ✔',
+                    text: req.body.body,
+                    html: '<p><b>MapLinkr</b> click link to open this MapLinkr map in browser</p>'
+                };
+
+                transporter.sendMail(message, (err, info) => {
+                    if (err) {
+                        console.log('Error occurred. ' + err.message);
+                        return process.exit(1);
+                    }
+
+                    console.log('Message sent: %s', info.messageId);
+                    // Preview only available when sending through an Ethereal account
+                    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+                });
+            });
+        }
     };
 
 
